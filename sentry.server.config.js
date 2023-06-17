0b64995b62084156426780c7e72d68aecfa6ef6e
TruongPhatTrienB1910009 <phattrientruong15062001@gmail.com>
2023-06-17 12:31:04 +0700

    config sentry

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -17,3 +17,20 @@ init({
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
   // that it will also get attached to your source maps
 })
+
+
+// Sentry.init({
+//   dsn: "https://d32d60915c7f40e59f8e26696c2040ad@o4505372823322624.ingest.sentry.io/4505372825681920",
+//   integrations: [
+//     new Sentry.BrowserTracing({
+//       // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+//       tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+//     }),
+//     new Sentry.Replay(),
+//   ],
+//   // Performance Monitoring
+//   tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+//   // Session Replay
+//   replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+//   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+// });
